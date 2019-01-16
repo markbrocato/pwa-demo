8993d9b1b46cad3b985c52f5cc164716c4101902
Mark Brocato <mark.brocato@moovweb.com>
2019-01-16 11:13:24 -0800

    initial commit

---
+++ src/proxy/renderHeader.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../header/Header'
+import { render } from 'react-storefront/renderers'
+import AppModel from '../AppModel'
+import theme from '../theme'
+import createGlobalState from '../globalState'
+
+/**
+ * Inserts the PWA header into adapt pages.
+ * @param {Object} stats Webpack build stats object for the client build
+ */
+export default function renderHeader(stats) {
+  const { html } = render({
+    component: <Header showPromo={false}/>,
+    state: AppModel.create(createGlobalState()),
+    theme,
+    stats,
+    clientChunk: 'header' // the name of the entry injected into config/web.dev.*.js
+  })
+
+  // remove the existing header
+  $body.find('#header').remove()
+  $body.find('script').remove()
+
+  // add the new header and supporting resources to the document
+  const $header = $(tag('div', { class: 'mw-header' })).append(html)
+  $body.prepend($header)
+}
