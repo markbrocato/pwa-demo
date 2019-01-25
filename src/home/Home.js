b65651661bc1c42724c14e6437c3deafcd890bb7
Mark Brocato <mark.brocato@moovweb.com>
2019-01-25 09:44:59 -0800

    improved PDP

--- src/home/Home.js
+++ src/home/Home.js
@@ -21,7 +21,7 @@ export default class Home extends Component {
         </Row>
         <Row>
           <Typography variant="subtitle1">
-            <CmsSlot>{ app.welcomeMessage }</CmsSlot>
+            Now with enhanced PDP!
           </Typography>
         </Row>
       </Container> 
